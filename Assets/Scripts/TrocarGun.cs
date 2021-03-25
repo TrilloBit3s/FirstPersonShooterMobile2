3cc3e23353c167616f6d844df76a825561a569ef
TrilloBit3s <TrilloBit3s@gmail.com>
2021-03-25 01:56:11 -0300

    First Commit

---
+++ Assets/Scripts/TrocarGun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrocarGun : MonoBehaviour
+{
+    public GameObject ObjectGun;
+    public GameObject ObjectGun2;
+
+    private bool trocar;
+    private bool pressionado;
+
+    public void TrocarGuns()
+    {
+        trocar = !trocar;
+        if(pressionado == trocar)
+        {
+            TrocarGun1();
+        }
+        else
+        {
+            TrocarGun2();
+        }
+    }
+        
+    void TrocarGun1()
+    {
+       ObjectGun.SetActive(true);
+       ObjectGun2.SetActive(false);
+    }
+
+    void TrocarGun2()
+    {
+        ObjectGun.SetActive(false);
+        ObjectGun2.SetActive(true);
+    }
+}
